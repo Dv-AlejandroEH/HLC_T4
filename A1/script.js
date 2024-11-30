a3877f26ab6f086e62e82b5ef74cefcf501a086a
Dv-AlejandroEH <aespher121@gmail.com>
2024-11-30 16:11:43 +0100

    JS

--- A1/script.js
+++ A1/script.js
@@ -1,10 +1,33 @@
 function cambiarColor(tema){
-    tema = document.getElementById('tema');
+    let header = document.getElementById('header');
+    let body = document.getElementById('body');
+    let textarea = document.getElementById('textarea');
+    let input = document.getElementById('input');
+    let h1 = document.getElementById('h1');
+    let h2 = document.getElementById('h2');
     switch (tema) {
         case 'oscuro':
-            let header = document.getElementById('header');
-            header.style.backgroundColor = 'gray';
+            input.className = 'oscuro';
+            textarea.className = 'oscuro';
+            body.className = 'oscuro';
+            header.className = 'oscuro';
+            break;
+        case 'claro':
+            header.className = 'claro';
+            break;
+        case 'azul':
+            input.className = 'azul';
+            textarea.className = 'azul';
+            body.className = 'azul';
+            header.className = 'azul';
+            h1.className = 'azul';
+            h2.className = 'azul';
+            break;
+        case 'rosa':
+            header.className = 'rosa';
+            break;
+        case 'verde':
+            header.className = 'verde';
             break;
     }
-    
 }
